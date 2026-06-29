949115a862e595b970a69c701c7bef7713617e4d
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-28 20:12:16 -0800

    Upgrade project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.composenova"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.example.composenova"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
