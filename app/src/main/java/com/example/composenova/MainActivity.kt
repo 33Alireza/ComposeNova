fec59cf1e65fff9b68d17bf196b6ca71f27d169d
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-29 20:22:05 -0800

    Implement column example

--- app/src/main/java/com/example/composenova/MainActivity.kt
+++ app/src/main/java/com/example/composenova/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -19,14 +23,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeNovaTheme {
-                Text(
-                    modifier = Modifier
-                        .background(Color(0xFFE91E63))
-                        .padding(16.dp),
-                    text = "Hello Compose",
-                    color = Color(0xFF2196F3),
-                    fontSize = 30.sp,
-                )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+
+                    Text(
+                        modifier = Modifier
+                            .background(Color(0xFFE91E63))
+                            .padding(16.dp),
+                        text = "Hello Compose",
+                        color = Color(0xFF2196F3),
+                        fontSize = 30.sp,
+                    )
+
+                    Text(
+                        modifier = Modifier
+                            .background(Color(0xFFE91E63))
+                            .padding(16.dp),
+                        text = "Hello Android",
+                        color = Color(0xFF2196F3),
+                        fontSize = 30.sp,
+                    )
+                }
             }
         }
     }
