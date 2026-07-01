e9d08519e4e79751748919e4221235f1131bc32a
Alireza Yousefzadeh <thegeisha2006@gmail.com>
2026-06-30 19:11:51 -0800

    Implement image example in condition

--- app/src/main/java/com/example/composenova/MainActivity.kt
+++ app/src/main/java/com/example/composenova/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import com.example.composenova.ui.theme.ComposeNovaTheme
 
@@ -17,11 +14,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeNovaTheme {
-                Image(
-                    modifier = Modifier.background(Color(0xFF4CAF50)),
-                    painter = painterResource(R.drawable.ic_launcher_foreground),
-                    contentDescription = null,
-                )
+                for (i in 1..10){
+                    Image(
+                        painter = painterResource(R.drawable.ic_launcher_foreground),
+                        contentDescription = null
+                    )
+                }
             }
         }
     }
